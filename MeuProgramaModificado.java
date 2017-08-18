395d034a309e4758cab7cd556a9ee428eb25e280
wilsonalexandrebaez <wab.estacio@gmail.com>
2017-08-18 08:46:56 -0400

    Aulas da Alura

---
+++ MeuProgramaModificado.java
@@ -0,0 +1,6 @@
+class MeuProgramaModificado {
+	public static void main ( String [] args){
+		System.out.print ("Minha Mensagem escrita aqui!!!");
+		System.out.println("Minha segunda linha!!");
+	}
+}
